e5b13951f0ce1fe7faa4211ce67eac29baedf830
alexfuentes <fuentes.alex@gmail.com>
2020-07-22 09:47:51 -0400

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
+use futures::{FutureExt, StreamExt};
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+    pretty_env_logger::init();
 
-    warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+    let routes = warp::path("echo")
+        // The `ws()` filter will prepare the Websocket handshake.
+        .and(warp::ws())
+        .map(|ws: warp::ws::Ws| {
+            // And then our closure will be called when it completes...
+            ws.on_upgrade(|websocket| {
+                // Just echo all messages back...
+                let (tx, rx) = websocket.split();
+                rx.forward(tx).map(|result| {
+                    if let Err(e) = result {
+                        eprintln!("websocket error: {:?}", e);
+                    }
+                })
+            })
+        });
+    
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
